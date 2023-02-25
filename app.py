a773dc026d1848d12bd81937de10d6b59e3a7828
Yogesh Prabhu <yogeshpra318@gmail.com>
2023-02-25 10:35:45 -0600

    flask app

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request, jsonify
+import nltk
+import pickle
+import numpy as np
+from nltk.corpus import stopwords
+import re
+import os
+from nltk.stem.porter import PorterStemmer
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__, template_folder='.')
+ps = PorterStemmer()
+# Load model and vectorizer
+model_filename = 'model2.pkl'
+tfidfvect_filename = 'tfidfvect2.pkl'
+model_path = os.path.join(basedir, model_filename)
+tfidfvect_path = os.path.join(basedir, tfidfvect_filename)
+
+
+model = pickle.load(open(model_path, 'rb'))
+tfidfvect = pickle.load(open(tfidfvect_path, 'rb'))
+
+# Build functionalities
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+def predict(text):
+    review = re.sub('[^a-zA-Z]', ' ', text)
+    review = review.lower()
+    review = review.split()
+    review = [ps.stem(word) for word in review if not word in stopwords.words('english')]
+    review = ' '.join(review)
+    review_vect = tfidfvect.transform([review]).toarray()
+    score = model.decision_function(review_vect)
+    proba = 1 / (1 + np.exp(-score))
+    prediction = 'FAKE' if model.predict(review_vect) == 0 else 'REAL'
+    return prediction, proba[0]
+
+@app.route('/', methods=['POST'])
+def webapp():
+    text = request.form['text']
+    prediction, probability = predict(text)
+
+    probability = f"{probability.max()*100:.2f}%"
+
+    return render_template('index.html', text=text, result=prediction, probability=probability)
+
+@app.route('/predict/', methods=['GET','POST'])
+def api():
+    text = request.args.get("text")
+    prediction = predict(text)
+    return jsonify(prediction=prediction)
+if __name__ == "__main__":
